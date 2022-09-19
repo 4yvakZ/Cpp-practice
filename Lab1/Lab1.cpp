1fd667b0cc5e32a8d582435d94973fdb907ff0e9
4yvakZ <35529078+4yvakZ@users.noreply.github.com>
2022-09-19 10:49:45 +0300

    Update Lab1.cpp

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -107,70 +107,68 @@ int main() {
     city.hungryCitizens = 0;
     city.plantedLand = 0;
     int round = 0;
-    while (true) {
-        int round = 0;
-        while (round < kLastRound) {
-            int landPrice = rand() % (kMaxLandPrice - kMinLandPrice + 1) + kMinLandPrice;
-            if (round == 0) {
-                std::cout << "Мой повелитель, соизволь поведать тебе о твоём городе перед началом твоего велечайшего правления\n";
-                std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
-                std::cout << "\tВ амбарах " << city.wheat << " бушеля пшеницы;\n";
-                std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
-                std::cout << "\t1 акр земли стоит " << landPrice << " бушель.\n";
-            } else {
-                int wheatFromOneLand = rand() % (kMaxWheatFormLand - kMinWheatFormLand + 1) + kMinWheatFormLand;
-                int wheatGrown = wheatFromOneLand * city.plantedLand;
-                city.wheat += wheatGrown;
-                int wheatEatedByRats = rand() % (int)(city.wheat * kMaxWheatEatenByRats - city.wheat * kMinWheatEatenByRats + 1) + city.wheat * kMinWheatEatenByRats;
-                city.wheat -= wheatEatedByRats;
 
-                int newSitizens = city.hungryCitizens / 2 + (5 - wheatFromOneLand) * city.wheat / 600 + 1;
-                if (newSitizens < 0) {
-                    newSitizens = 0;
-                } else if (newSitizens > 50) {
-                    newSitizens = 50;
-                }
+    while (round < kLastRound) {
+        int landPrice = rand() % (kMaxLandPrice - kMinLandPrice + 1) + kMinLandPrice;
+        if (round == 0) {
+            std::cout << "Мой повелитель, соизволь поведать тебе о твоём городе перед началом твоего велечайшего правления\n";
+            std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
+            std::cout << "\tВ амбарах " << city.wheat << " бушеля пшеницы;\n";
+            std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
+            std::cout << "\t1 акр земли стоит " << landPrice << " бушель.\n";
+        } else {
+            int wheatFromOneLand = rand() % (kMaxWheatFormLand - kMinWheatFormLand + 1) + kMinWheatFormLand;
+            int wheatGrown = wheatFromOneLand * city.plantedLand;
+            city.wheat += wheatGrown;
+            int wheatEatedByRats = rand() % (int)(city.wheat * kMaxWheatEatenByRats - city.wheat * kMinWheatEatenByRats + 1) + city.wheat * kMinWheatEatenByRats;
+            city.wheat -= wheatEatedByRats;
 
-                city.citizens += newSitizens - city.hungryCitizens;
-
-                std::cout << "Мой повелитель, соизволь поведать тебе\n";
-                std::cout << "\tв году " << round << " твоего высочайшего правления\n";
-                std::cout << "\t";
-                if (city.hungryCitizens != 0) {
-                    std::cout << city.hungryCitizens << " человек умерли с голоду, и ";
-                }
-                std::cout << newSitizens << " человек прибыли в наш великий город;\n";
-                if (rand() % 100 < kPercentProbobilityOfPlague) {
-                    std::cout << "\tЧума уничтожила половину населения;\n";
-                    city.citizens /= 2;
-                }
-                std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
-                std::cout << "\tМы собрали " << wheatGrown << " бушеля пшеницы, по " << wheatFromOneLand << " бушеля с акра;\n";
-                std::cout << "\t";
-                if (wheatEatedByRats > 0) {
-                    std::cout << "Крысы истребили " << wheatEatedByRats << " бушеля пшеницы, оставив ";
-                }
-                std::cout << city.wheat << " бушеля пщеницы в амбарах;\n";
-                std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
-                std::cout << "\t1 акр земли стоит " << landPrice << " бушель.\n";
+            int newSitizens = city.hungryCitizens / 2 + (5 - wheatFromOneLand) * city.wheat / 600 + 1;
+            if (newSitizens < 0) {
+                newSitizens = 0;
+            } else if (newSitizens > 50) {
+                newSitizens = 50;
             }
 
-            std::cout << "Что пожелаешь, повелитель?\n";
+            city.citizens += newSitizens - city.hungryCitizens;
 
-            buyLand(city, landPrice);
-
-            sellLand(city, landPrice);
-
-            feedCity(city);
-
-            plantLand(city);
-
-            if (city.hungryCitizens * 1.0 / city.citizens > 0.45) {
-                std::cout << "Из-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города.Теперь вы вынуждены влачить жалкое существование в изгнании\n";
-                break;
+            std::cout << "Мой повелитель, соизволь поведать тебе\n";
+            std::cout << "\tв году " << round << " твоего высочайшего правления\n";
+            std::cout << "\t";
+            if (city.hungryCitizens != 0) {
+                std::cout << city.hungryCitizens << " человек умерли с голоду, и ";
             }
-
-            round++;
+            std::cout << newSitizens << " человек прибыли в наш великий город;\n";
+            if (rand() % 100 < kPercentProbobilityOfPlague) {
+                std::cout << "\tЧума уничтожила половину населения;\n";
+                city.citizens /= 2;
+            }
+            std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
+            std::cout << "\tМы собрали " << wheatGrown << " бушеля пшеницы, по " <<  wheatFromOneLand << " бушеля с акра;\n";
+            std::cout << "\t";
+            if (wheatEatedByRats > 0) {
+                std::cout << "Крысы истребили " << wheatEatedByRats << " бушеля пшеницы, оставив ";
+            }
+            std::cout << city.wheat << " бушеля пщеницы в амбарах;\n";
+            std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
+            std::cout << "\t1 акр земли стоит " << landPrice << " бушель.\n";
         }
+
+        std::cout << "Что пожелаешь, повелитель?\n";
+
+        buyLand(city, landPrice);
+
+        sellLand(city, landPrice);
+
+        feedCity(city);
+
+        plantLand(city);
+
+        if (city.hungryCitizens * 1.0 / city.citizens > 0.45) {
+            std::cout << "Из-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города.Теперь вы вынуждены влачить жалкое существование в изгнании\n";
+            break;
+        }
+
+        round++;
     }
 }
