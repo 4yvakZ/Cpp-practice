4c85352f1b877016f7365501e28a9fc14c350377
4yvakZ <35529078+4yvakZ@users.noreply.github.com>
2022-09-21 10:13:57 +0300

    Lab1 Done

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <Windows.h>
 
 constexpr int kLastRound = 10;
 
@@ -23,12 +26,23 @@ constexpr int kStartNumberOfCitisens = 100;
 constexpr int kStartNumberOfWheat = 2800;
 constexpr int kStartNumberOfLand = 1000;
 
+const std::string kSaveFileName = "save.txt";
+
 struct City {
-    int citizens;
-    int wheat;
-    int land;
-    int hungryCitizens;
-    int plantedLand;
+    int citizens = kStartNumberOfCitisens;
+    int wheat = kStartNumberOfWheat;
+    int land = kStartNumberOfLand;
+    int hungryCitizens = 0;
+    int plantedLand = 0;
+};
+
+struct RoundInfo {
+    int round = 0;
+    int wheatFromOneLand = 0;
+    int wheatEatedByRats = 0;
+    int landPrice = 0;
+    bool isPlague = 0;
+    float sumPercentageOfDeath = 0;
 };
 
 void buyLand(City &city, int landPrice) {
@@ -96,69 +110,143 @@ void plantLand(City &city) {
     }
 }
 
+void saveRound(City city, RoundInfo roundInfo){
+    std::ofstream fout(kSaveFileName, std::ios_base::out | std::ios_base::trunc);
+    
+    fout << roundInfo.round << " " << roundInfo.isPlague << " " << roundInfo.wheatEatedByRats << " " 
+        << roundInfo.wheatFromOneLand << " " << roundInfo.landPrice << " " << roundInfo.sumPercentageOfDeath << " ";
+    fout << city.citizens << " " << city.hungryCitizens << " " << city.land << " " << city.plantedLand << " " << city.wheat;
+
+    fout.close();
+}
+
+bool loadRound(City& city, RoundInfo &roundInfo) {
+    std::ifstream fin(kSaveFileName);
+    if (!fin.is_open()) {
+        return false;
+    }
+
+    fin >> roundInfo.round >> roundInfo.isPlague >> roundInfo.wheatEatedByRats >> roundInfo.wheatFromOneLand 
+        >> roundInfo.landPrice >> roundInfo.sumPercentageOfDeath;
+    fin >> city.citizens >> city.hungryCitizens >> city.land >> city.plantedLand >> city.wheat;
+
+    fin.close();
+    return true;
+}
+
+void initialInfoPrint(City city, RoundInfo roundInfo) {
+    std::cout << "Мой повелитель, соизволь поведать тебе о твоём городе перед началом твоего велечайшего правления\n";
+    std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
+    std::cout << "\tВ амбарах " << city.wheat << " бушеля пшеницы;\n";
+    std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
+    std::cout << "\t1 акр земли стоит " << roundInfo.landPrice << " бушель.\n";
+}
+
+void printInfo(City city, RoundInfo roundInfo, int newSitizens, int wheatGrown) {
+    std::cout << "Мой повелитель, соизволь поведать тебе\n";
+    std::cout << "\tв году " << roundInfo.round << " твоего высочайшего правления\n";
+    std::cout << "\t";
+    if (city.hungryCitizens != 0) {
+        std::cout << city.hungryCitizens << " человек умерли с голоду, и ";
+    }
+    std::cout << newSitizens << " человек прибыли в наш великий город;\n";
+
+    if (roundInfo.isPlague) {
+        std::cout << "\tЧума уничтожила половину населения;\n";
+    }
+    std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
+    std::cout << "\tМы собрали " << wheatGrown << " бушеля пшеницы, по " << roundInfo.wheatFromOneLand << " бушеля с акра;\n";
+    std::cout << "\t";
+    if (roundInfo.wheatEatedByRats > 0) {
+        std::cout << "Крысы истребили " << roundInfo.wheatEatedByRats << " бушеля пшеницы, оставив ";
+    }
+    std::cout << city.wheat << " бушеля пщеницы в амбарах;\n";
+    std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
+    std::cout << "\t1 акр земли стоит " << roundInfo.landPrice << " бушель.\n";
+}
+
 int main() {
-    setlocale(LC_ALL, "Russian");
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     std::srand(std::time(nullptr));
 
     City city;
-    city.citizens = kStartNumberOfCitisens;
-    city.wheat = kStartNumberOfWheat;
-    city.land = kStartNumberOfLand;
-    city.hungryCitizens = 0;
-    city.plantedLand = 0;
-    int round = 0;
+    RoundInfo roundInfo;
+    bool isSaveLoaded = false;
+    bool isHungryGameOver = false;
+    float sumPercentageOfDeath = 0;
+    
+    if (loadRound(city, roundInfo)) {
+        while (true) {
+            std::cout << "Желаете продолжить сохранённую игру? [да/нет] ";
+            std::string answer;
+            answer = "да";
+            std::cin >> answer;
+            if (answer == "нет") {
+                roundInfo.round = 0;
+                roundInfo.sumPercentageOfDeath = 0;
+                city.citizens = kStartNumberOfCitisens;
+                city.wheat = kStartNumberOfWheat;
+                city.land = kStartNumberOfLand;
+                city.hungryCitizens = 0;
+                city.plantedLand = 0;
+                break;
+            } else if (answer == "да") {
+                isSaveLoaded = true;
+                break;
+            }
+        }
+    }
 
-    while (round < kLastRound) {
-        int landPrice = rand() % (kMaxLandPrice - kMinLandPrice + 1) + kMinLandPrice;
-        if (round == 0) {
-            std::cout << "Мой повелитель, соизволь поведать тебе о твоём городе перед началом твоего велечайшего правления\n";
-            std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
-            std::cout << "\tВ амбарах " << city.wheat << " бушеля пшеницы;\n";
-            std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
-            std::cout << "\t1 акр земли стоит " << landPrice << " бушель.\n";
+    while (roundInfo.round < kLastRound) {
+        if (!isSaveLoaded) {
+            roundInfo.landPrice = rand() % (kMaxLandPrice - kMinLandPrice + 1) + kMinLandPrice;
+        }
+        if (roundInfo.round == 0) {
+            initialInfoPrint(city, roundInfo);
         } else {
-            int wheatFromOneLand = rand() % (kMaxWheatFormLand - kMinWheatFormLand + 1) + kMinWheatFormLand;
-            int wheatGrown = wheatFromOneLand * city.plantedLand;
-            city.wheat += wheatGrown;
-            int wheatEatedByRats = rand() % (int)(city.wheat * kMaxWheatEatenByRats - city.wheat * kMinWheatEatenByRats + 1) + city.wheat * kMinWheatEatenByRats;
-            city.wheat -= wheatEatedByRats;
 
-            int newSitizens = city.hungryCitizens / 2 + (5 - wheatFromOneLand) * city.wheat / 600 + 1;
+            if (!isSaveLoaded) {
+                roundInfo.wheatFromOneLand = rand() % (kMaxWheatFormLand - kMinWheatFormLand + 1) + kMinWheatFormLand;
+            }
+
+            int wheatGrown = roundInfo.wheatFromOneLand * city.plantedLand;
+
+            if (!isSaveLoaded) {
+                city.wheat += wheatGrown;
+                roundInfo.wheatEatedByRats = rand() % (int)(city.wheat * kMaxWheatEatenByRats - city.wheat * kMinWheatEatenByRats + 1) + city.wheat * kMinWheatEatenByRats;
+                city.wheat -= roundInfo.wheatEatedByRats;
+            }
+
+            int newSitizens = city.hungryCitizens / 2 + (5 - roundInfo.wheatFromOneLand) * city.wheat / 600 + 1;
             if (newSitizens < 0) {
                 newSitizens = 0;
             } else if (newSitizens > 50) {
                 newSitizens = 50;
             }
 
-            city.citizens += newSitizens - city.hungryCitizens;
+            if (!isSaveLoaded) {
+                roundInfo.sumPercentageOfDeath += 1.0 * city.hungryCitizens / city.citizens;
+                city.citizens += newSitizens - city.hungryCitizens;
+                roundInfo.isPlague = rand() % 100 < kPercentProbobilityOfPlague;
+                if (roundInfo.isPlague) {
+                    city.citizens /= 2;
+                }
+            }
 
-            std::cout << "Мой повелитель, соизволь поведать тебе\n";
-            std::cout << "\tв году " << round << " твоего высочайшего правления\n";
-            std::cout << "\t";
-            if (city.hungryCitizens != 0) {
-                std::cout << city.hungryCitizens << " человек умерли с голоду, и ";
-            }
-            std::cout << newSitizens << " человек прибыли в наш великий город;\n";
-            if (rand() % 100 < kPercentProbobilityOfPlague) {
-                std::cout << "\tЧума уничтожила половину населения;\n";
-                city.citizens /= 2;
-            }
-            std::cout << "\tНаселение города сейчас составляет " << city.citizens << " человек;\n";
-            std::cout << "\tМы собрали " << wheatGrown << " бушеля пшеницы, по " <<  wheatFromOneLand << " бушеля с акра;\n";
-            std::cout << "\t";
-            if (wheatEatedByRats > 0) {
-                std::cout << "Крысы истребили " << wheatEatedByRats << " бушеля пшеницы, оставив ";
-            }
-            std::cout << city.wheat << " бушеля пщеницы в амбарах;\n";
-            std::cout << "\tГород сейчас занимает " << city.land << " акров;\n";
-            std::cout << "\t1 акр земли стоит " << landPrice << " бушель.\n";
+            isSaveLoaded = false;
+
+            printInfo(city, roundInfo, newSitizens, wheatGrown);
+            
+            saveRound(city, roundInfo);
         }
 
         std::cout << "Что пожелаешь, повелитель?\n";
 
-        buyLand(city, landPrice);
+        buyLand(city, roundInfo.landPrice);
 
-        sellLand(city, landPrice);
+        sellLand(city, roundInfo.landPrice);
 
         feedCity(city);
 
@@ -166,9 +254,27 @@ int main() {
 
         if (city.hungryCitizens * 1.0 / city.citizens > 0.45) {
             std::cout << "Из-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города.Теперь вы вынуждены влачить жалкое существование в изгнании\n";
+            isHungryGameOver = true;
             break;
         }
 
-        round++;
+        roundInfo.round++;
     }
+    if (!isHungryGameOver) {
+        float p = roundInfo.sumPercentageOfDeath / kLastRound;
+        float l = city.land * 1.0 / city.citizens;
+        if (p > 0.33 && l < 7) {
+            std::cout << "Из-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города.Теперь вы вынуждены влачить жалкое существование в изгнании\n";
+        } else if (p > 10 && l < 9) {
+            std::cout << "Вы правили железной рукой, подобно Нерону и Ивану Грозному.Народ вздохнул с облегчением, и никто больше не желает видеть вас правителем\n";
+        } else if (p > 3 && l < 10) {
+            std::cout << "Вы справились вполне неплохо, у вас, конечно, есть недоброжелатели, но многие хотели бы увидеть вас во главе города снова\n ";
+        } else {
+            std::cout << "Фантастика! Карл Великий, Дизраэли и Джефферсон вместе не справились бы лучше\n ";
+        }
+    }
+
+    std::remove(kSaveFileName.c_str());
+
+    return 0;
 }
